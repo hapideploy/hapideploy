0f0d032de022cec7ca506d345cbd5e5db2acf17c
Quynh Nguyen <seriquynh@gmail.com>
2025-03-28 14:35:12 +0700

    Separate collection test into smaller parts

--- tests/test_support.py
+++ tests/test_support.py
@@ -5,7 +5,8 @@ from hapi.support import Collection, env_stringify, extract_curly_brackets
 
 
 class Person:
-    pass
+    def __init__(self, name: str):
+        self.name = name
 
 
 class Student(Person):
@@ -13,7 +14,8 @@ class Student(Person):
 
 
 class Language:
-    pass
+    def __init__(self, name: str):
+        self.name = name
 
 
 def test_env_stringify_function():
@@ -36,29 +38,33 @@ def test_extract_curly_brackets():
     assert keys == ["release_path", "bin/npm"]
 
 
-def test_collection():
+def test_collection_add():
+    collection = Collection(Person)
+
+    with pytest.raises(TypeError):
+        collection.add(Language("Python"))
+
+
+def test_collection_empty():
     collection = Collection(Person)
 
     assert collection.empty()
 
-    with pytest.raises(TypeError):
-        collection.add(Language())
+    collection.add(Person("John"))
 
-    p1 = Person()
-    p1.name = "John"
+    assert not collection.empty()
+
+
+def test_collection_filter_key():
+    collection = Collection(Person)
+
+    p1 = Person("John")
     collection.add(p1)
 
-    assert collection.empty() is False
-
-    p2 = Person()
-    p2.name = "Jane"
+    p2 = Person("Jane")
     collection.add(p2)
 
     collection.filter_key(lambda name, item: item.name == name)
-
-    assert collection.find("John") == p1
-    assert collection.find("Jane") == p2
-    assert collection.match(lambda item: item.name == "John") == p1
 
     with pytest.raises(ItemNotFound):
         collection.find("James")
@@ -66,6 +72,26 @@ def test_collection():
     with pytest.raises(ItemNotFound):
         collection.match(lambda item: item.name == "James")
 
-    assert collection.all() == [p1, p2]
+    assert collection.find("John") == p1
+    assert collection.find("Jane") == p2
+    assert collection.match(lambda item: item.name == "John") == p1
 
-    # TODO: test filter method
+
+def test_collection_filter():
+    collection = Collection(Person)
+
+    p1 = Person("John")
+    collection.add(p1)
+
+    p2 = Person("Jane")
+    collection.add(p2)
+
+    p3 = Student("James")
+    collection.add(p3)
+
+    assert collection.all() == [p1, p2, p3]
+    assert collection.filter(lambda p: p.name == "John") == [p1]
+    assert collection.filter(lambda p: p.name == "John" or p.name == "James") == [
+        p1,
+        p3,
+    ]
